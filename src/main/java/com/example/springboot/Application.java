3d522535ddf63656f029b3986d7cf68937273de8
milosun1995 <18838980210@163.com>
2017-05-22 19:28:19 +0800

    first commit

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,32 @@
+package com.example.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.springboot.property.HomeProperties;
+
+/**
+ * Spring Boot 应用启动类
+ * <p>
+ * Created by bysocket on 16/4/26.
+ */
+// Spring Boot 应用的标识
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private HomeProperties homeProperties;
+
+    public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        SpringApplication.run(Application.class, args);
+    }
+
+    public void run(String... args) throws Exception {
+        System.out.println("\n" + homeProperties.toString());
+        System.out.println();
+    }
+}
---
+++ src/test/java/com/example/springboot/web/HelloWorldControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.springboot.web;
+
+import org.junit.Test;
+
+import com.example.springboot.web.HelloWorldController;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Spring Boot HelloWorldController 测试 - {@link HelloWorldController}
+ *
+ * Created by bysocket on 16/4/26.
+ */
+public class HelloWorldControllerTest {
+
+    @Test
+    public void testSayHello() {
+        assertEquals("Hello,World!",new HelloWorldController().sayHello());
+    }
+}
